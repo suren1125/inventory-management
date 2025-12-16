ddd2df1e2ba96d38d010c9cda38f2c794305ad10
Suren Shrestha <saintsrs1125@gmail.com>
2025-12-16 18:33:41 +0545

    added popular products card and rating

--- server/dist/src/controllers/dashboardController.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Request, Response } from "express";
-export declare const getDashboardMetrics: (req: Request, res: Response) => Promise<void>;
-//# sourceMappingURL=dashboardController.d.ts.map
